a9e1a78c36c777faad9c91ca010d18c1ae9811c6
manonlvnr <manon.lavenier@gmail.com>
2021-01-23 16:47:03 +0100

    random computer response

--- shifumi.js
+++ shifumi.js
@@ -1,12 +1,13 @@
 // étape 1 choix pierre/feuille/ciseaux
 
 const { Select } = require('enquirer'); //require = librairie et entre parenthèse = API de la librairie
+const choices = ['rock', 'paper', 'cisors'];
 
 async function getUserChoice() {
   const prompt = new Select({
     name: 'element',
     message: 'chose your element',
-    choices: ['rock', 'paper', 'cisors'],
+    choices: choices.slice(),
   });
 
   const response = await prompt
@@ -18,9 +19,16 @@ async function getUserChoice() {
   return response;
 }
 
+function choose() {
+  var index = Math.floor(Math.random() * choices.length);
+  console.log('choices[index]: ', choices);
+  return choices[index];
+}
+
 async function main() {
-  const response = await getUserChoice();
-  //console.log('response: ', response);
+  const playerResponse = await getUserChoice();
+  const computerResponse = choose();
+  console.log('Computer response :', computerResponse);
 }
 
 main();
